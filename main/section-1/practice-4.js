8d5481d89538f466b4bc8fb438a63b88c1de0b1f
GalaxyXie <771472141@qq.com>
2019-07-11 12:35:43 +0800

    feat:implement practice-4

--- main/section-1/practice-4.js
+++ main/section-1/practice-4.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+
+    let A= collectionA.filter(function elementsInObjectB(num){
+
+              return objectB.value.indexOf(num.key)>-1;
+         });
+    let B=A.map(function getKey(num) {
+                    return num.key;
+                });
+    console.log(B);
+    return B;
 }
